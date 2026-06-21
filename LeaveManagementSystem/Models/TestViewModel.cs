f7d7916cee0d68529d1cd1fadfb650ab5620c934
John-Zevgolis <zevgolisgiannis@yahoo.gr>
2026-06-22 02:20:10 +0300

    First commit

---
+++ LeaveManagementSystem/Models/TestViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
